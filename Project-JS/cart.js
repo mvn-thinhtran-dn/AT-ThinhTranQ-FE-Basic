fe4667fd34f1cd8fec075ab0cd68cff5fa5c6940
at-thinhtran <thinh.tran@asiantech.vn>
2020-01-14 09:59:26 +0700

    Task-20200114-fix cart-list-table

--- Project-JS/cart.js
+++ Project-JS/cart.js
@@ -38,20 +38,14 @@ var cart = {
       // List items
       var total = 0, subtotal = 0;
       for (var i in cart.data) {
-        item = document.createElement('div');
+        item = document.createElement('tr');
         item.classList.add('c-item');
         product = cart.data[i];
 
         // Name
-        part = document.createElement('span');
+        part = document.createElement('td');
         part.innerHTML = product['name'];
         part.classList.add('c-name');
-        item.appendChild(part);
-
-        // Product Description
-        part = document.createElement('span');
-        part.innerHTML = product['desc'];
-        part.classList.add('c-desc');
         item.appendChild(part);
 
         // Quantity
@@ -62,9 +56,9 @@ var cart = {
         part.classList.add('c-qty');
         part.addEventListener('change', cart.change);
         item.appendChild(part);
-        
+
         // Product Price
-        part = document.createElement('span');
+        part = document.createElement('td');
         part.innerHTML = '$' +product['price'];
         part.classList.add('c-price');
         item.appendChild(part);
@@ -73,22 +67,24 @@ var cart = {
         subtotal = product['qty'] * product['price'];
         total += subtotal;
         container.appendChild(item);
+        part = document.createElement('td');
+        part.innerHTML = '$' + subtotal;
+        part.classList.add('c-subtotal');
+        item.appendChild(part);
+
+        //Delete
+        part = document.createElement('td');
+        part.innerHTML = '$' +product['price'];
+        part.classList.add('c-price');
+        item.appendChild(part);
       }
 
-      // // EMPTY BUTTONS
-      // item = document.createElement("input");
-      // item.type = "button";
-      // item.value = "Empty";
-      // item.addEventListener("click", cart.reset);
-      // item.classList.add("c-empty");
-      // container.appendChild(item);
-
-      // CHECKOUT BUTTONS
-      item = document.createElement("span");
-      item.type = "total";
-      item.value = "Checkout - " + "$" + total;
-      item.classList.add("c-checkout");
-      container.appendChild(item);
+      // CHECKOUT TOTAL
+      part = document.createElement('tr');
+      part.innerHTML = 'Total: ' + '$' + total;
+      part.classList.add('c-total');
+      item.appendChild(part);
+      
     }
   },
 
